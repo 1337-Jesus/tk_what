7476fa500d963bcd4cecd53ce2d9fa637a904e1b
Patrick G <patrick@network.cum>
2021-01-13 09:29:18 +0100

    add frames and entry box

--- start_tk.py
+++ start_tk.py
@@ -3,8 +3,22 @@
 import tkinter as tk
 
 window = tk.Tk()
-message = tk.Label(text="Dafuq is a window?", width=30)
 
-message.pack()
+frm_mac = tk.Frame()
+lbl_mac = tk.Label(text="Enter MAC Address: ", width=30, master = frm_mac)
+ent_mac = tk.Entry(width = 40, master = frm_mac)
+mac = ent_mac.get()
+
+frm_ip = tk.Frame()
+lbl_ip = tk.Label(text="Enter IP Address: ", width=30, master = frm_ip)
+ent_ip = tk.Entry(width = 40, master = frm_ip)
+ip = ent_ip.get()
+
+lbl_mac.pack()
+lbl_ip.pack()
+ent_mac.pack()
+ent_ip.pack()
+frm_mac.pack()
+frm_ip.pack()
 
 window.mainloop()
